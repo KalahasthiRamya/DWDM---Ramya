eeb8eb7f17075eed5d079fd3abfa7601f66cc7f4
Kalahasthi Ramya <156272585+KalahasthiRamya@users.noreply.github.com>
2025-02-17 10:20:01 +0530

    Add files via upload

---
+++ Ex. No -4 R-Division [192311149].R
@@ -0,0 +1,4 @@
+num1=as.integer(readline(prompt="Enter 1st no : "))
+num2=as.integer(readline(prompt="Enter 2nd no : "))
+num3=num1/num2
+print(num3)
---
+++ Ex. No -5 R-Odd or Even [192311149].R
@@ -0,0 +1,6 @@
+num1=as.integer(readline(prompt="Enter the no : "))
+if((num1%%2)==0){
+  print("Number is Even")
+}else{
+  print("Number is Odd")
+}
